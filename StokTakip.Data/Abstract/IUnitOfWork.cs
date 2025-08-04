b0a82d8ed5cfe77607820740b4fd52e0c6f3872f
Mahmut Baglama <mahmutbaglama98@gmail.com>
2025-08-04 17:45:20 +0300

    Unit of Work tasarım deseni kullaniliyor.

---
+++ StokTakip.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakip.Data.Abstract
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IDepoRepository Depo { get; }
+        ICariRepository Cari { get; }
+        IStokRepository Stok { get; }
+        IIrsaliyeRepository Irsaliye { get; }
+
+        IDepoTransferDetayRepository DepoTransferDetay { get; }
+        IIrsaliyeDetayRepository IrsaliyeDetay { get; }
+        IKullaniciRepository Kullanici { get; } 
+        IStokRepository Stoklar { get; }
+        ILogTakipRepository LogTakip { get; }
+        IMalzemeRepository Malzeme { get; }
+
+        Task<int> SaveAsync();
+
+
+
+
+
+    }
+}
