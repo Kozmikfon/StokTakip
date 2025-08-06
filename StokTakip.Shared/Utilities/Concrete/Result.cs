d92cb05618138465fd2d91292829ca8bc9a7aa86
Mahmut Baglama <mahmutbaglama98@gmail.com>
2025-08-06 09:34:16 +0300

    Result sınıfı ile devam eden işlemlerin durumunu görmek için yapıldı.

--- StokTakip.Shared/Utilities/Concrete/Result.cs
+++ StokTakip.Shared/Utilities/Concrete/Result.cs
@@ -10,6 +10,22 @@ namespace StokTakip.Shared.Utilities.Concrete
 {
     public class Result : IResult
     {
+        public Result(ResultStatus resultStatus)
+        {
+            ResultStatus = resultStatus;
+        }
+        public Result(ResultStatus resultStatus,string info)
+        {
+            ResultStatus = resultStatus;
+            Info = info;
+        }
+        public Result(ResultStatus resultStatus, string info,Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Info = info;
+            Exception=exception;
+        }
+
         public ResultStatus ResultStatus { get; }
 
         public string Info { get; }
