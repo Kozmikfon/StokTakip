12330f4ca3df6affcf007c6cb7971bedbb97fbe5
Mahmut Baglama <mahmutbaglama98@gmail.com>
2025-08-06 09:48:28 +0300

    Data Result artı olarak veri döndürme işlemi yapmaktadır.

---
+++ StokTakip.Shared/Utilities/Concrete/DataResult.cs
@@ -0,0 +1,47 @@
+﻿using StokTakip.Shared.Utilities.Abstract;
+using StokTakip.Shared.Utilities.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace StokTakip.Shared.Utilities.Concrete
+{
+    public class DataResult<T> : IDataResult<T>
+    {
+        public DataResult(ResultStatus resultStatus,T data)
+        {
+            ResultStatus = resultStatus;
+            Data = data;
+        }
+        public DataResult(ResultStatus resultStatus, string info, T data)
+        {
+            ResultStatus = resultStatus;
+            Info = info;
+            Data = data;
+            
+        }
+        public DataResult(ResultStatus resultStatus, string info, T data, Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Info = info;
+            Data = data;
+            Exception = exception;
+        }
+
+        public T Data { get; }
+
+        public ResultStatus ResultStatus { get; }
+
+        public string Info { get; }
+
+        public Exception Exception { get; }
+
+        //new DataResult<DepoDTO>(ResultStatus.Error,"",Data);
+
+
+        
+    }
+}
