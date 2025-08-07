9d6c1d038cec0a7edf2ad672e3f826964e466e83
Mahmut Baglama <mahmutbaglama98@gmail.com>
2025-08-07 16:27:54 +0300

    Irsaliye servis katmani olusturukuyor.

---
+++ StokTakip.Service/Abstract/IIrsaliyeService.cs
@@ -0,0 +1,26 @@
+﻿using StokTakip.Entities.Dtos.IrsaliyeDtos;
+using StokTakip.Shared.Utilities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakip.Service.Abstract
+{
+    public interface IIrsaliyeService
+    {
+        Task<IDataResult<IrsaliyeDto>> Get(int id);
+        Task<IDataResult<IrsaliyeDto>> Create(IrsaliyeCreateDto irsaliyeCreateDto);
+        Task<IDataResult<IrsaliyeDto>> Update(IrsaliyeUpdateDto irsaliyeUpdateDto);
+        Task<IResult> Delete(int id);
+        Task<IDataResult<List<IrsaliyeListDto>>> GetAllAsync();//tüm kayutlar
+        Task<IDataResult<List<IrsaliyeListDto>>> GetAllByMalzemeIdAsync(int malzemeId);
+        Task<IDataResult<List<IrsaliyeListDto>>> GetAllByDepoIdAsync(int depoId);
+        Task<IDataResult<List<IrsaliyeListDto>>> GetAllByMalzemeIdAndDepoIdAsync(int malzemeId, int depoId);
+
+        Task<IDataResult<List<IrsaliyeListDto>>> GetAllByNonDeleteAsync(); //silinmemiş kayıtlat
+        Task<IDataResult<List<IrsaliyeListDto>>> GetAllByNonDeleteAndActionAsync();    // hem silinmemiş hemde aktif olan kayıtlar
+
+    }
+}
---
+++ StokTakip.Service/Concrete/IrsaliyeService.cs
@@ -0,0 +1,64 @@
+﻿using StokTakip.Entities.Dtos.IrsaliyeDtos;
+using StokTakip.Service.Abstract;
+using StokTakip.Shared.Utilities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StokTakip.Service.Concrete
+{
+    public class IrsaliyeService : IIrsaliyeService
+    {
+        public Task<IDataResult<IrsaliyeDto>> Create(IrsaliyeCreateDto irsaliyeCreateDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IResult> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<IrsaliyeDto>> Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<List<IrsaliyeListDto>>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<List<IrsaliyeListDto>>> GetAllByDepoIdAsync(int depoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<List<IrsaliyeListDto>>> GetAllByMalzemeIdAndDepoIdAsync(int malzemeId, int depoId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<List<IrsaliyeListDto>>> GetAllByMalzemeIdAsync(int malzemeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<List<IrsaliyeListDto>>> GetAllByNonDeleteAndActionAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<List<IrsaliyeListDto>>> GetAllByNonDeleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IDataResult<IrsaliyeDto>> Update(IrsaliyeUpdateDto irsaliyeUpdateDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
